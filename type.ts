21528054e3c9b0c8afff2944584d3bcaf1f91773
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-08-03 22:34:58 +0330

    Feature: The instinct of TypeScript is add.

--- type.ts
+++ type.ts
@@ -3,9 +3,9 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     return showResult ? phrase + result : n1*n2;
 }
 
-const num1 = 5;
-const num2 = 7.1;
-const printResult = true;
-const resultPhrase = "Result is:"
+const num1: number = 5;
+const num2: number = 7.1;
+const printResult: boolean = true;
+const resultPhrase: string = "Result is:"
 const result = add(num1, num2, printResult, resultPhrase);
 console.log(result)
