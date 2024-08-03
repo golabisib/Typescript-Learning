af645cb5e9c8885552dec924527bdf154dac5c1c
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-08-03 23:25:48 +0330

    Fix: The Array Type is add.

--- type2.js
+++ type2.js
@@ -1,5 +1,9 @@
 var person = {
     age: 22,
     name: "Roozbeh",
+    hobbies: ["Sport", "Coding"],
 };
+var favoriteBooks;
+favoriteBooks = ["golabiBook", "sibBook"], 2;
 console.log(person.name);
+person.hobbies.map(function (hobby) { return console.log(hobby.toLowerCase()); });
--- type2.ts
+++ type2.ts
@@ -1,9 +1,15 @@
 const person: {
     age: number;
     name: string;
+    hobbies: string[];
 } = {
     age: 22,
     name: "Roozbeh",
+    hobbies: ["Sport", "Coding"],
 }
+let favoriteBooks: (string | number)[];
+favoriteBooks = ["golabiBook", "sibBook"], 2
 
 console.log(person.name)
+
+person.hobbies.map(hobby => console.log(hobby.toLowerCase()))
