bb9814bd7e704dfc8e42c34693d85dea203dddc0
Roozbeh Rigi Jangjo <silentroozbeh@gmail.com>
2024-08-06 22:07:51 +0330

    Fix: The union types is add.

---
+++ type5.ts
@@ -0,0 +1,13 @@
+function combine(input1: number | string, input2: number | string) {
+    let result : number | string ;
+    if (typeof input1 === "number" && typeof input2 === "number"){
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+console.log(combine(2, 3))
+console.log(combine("golabi", 3))
+console.log(combine("golabi", "sib"))
